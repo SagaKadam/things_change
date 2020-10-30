e31d5fb8f5cb2f8b6e0465fc2e3e43fe1f63f675
Saga_Kadam <ksagar.sk23@gmail.com>
2020-10-30 12:48:13 +0530

    before redux application state

---
+++ react-redux-tutorials/react-redux/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    age: 21
+  }
+
+  onAgeUp = () => {
+    this.setState({
+      ...this.state,
+      age: ++this.state.age
+    })
+  }
+
+  onAgeDown = () => {
+    this.setState({
+      ...this.state,
+      age: --this.state.age
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          Age: <span>{this.state.age}</span>
+        </div>
+        <button onClick={this.onAgeUp}>Age Up</button>
+        <button onClick={this.onAgeDown}>Age DOWN</button>
+      </div>
+    );
+  }
+}
+
+export default App;
